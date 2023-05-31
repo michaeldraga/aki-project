0f66eb5617ddabbbec63790267c2a61c1fc09f5f
Michael Draga <midall01.md@gmail.com>
2023-05-31 21:55:58 +0200

    Small cleanup before hand in

--- wgan_gradient_penalty.py
+++ wgan_gradient_penalty.py
@@ -1,4 +1,4 @@
-# stolen from https://github.com/Zeleni9/pytorch-wgan/blob/master/models/wgan_gradient_penalty.py#L296
+# stolen from https://github.com/Zeleni9/pytorch-wgan/blob/master/models/wgan_gradient_penalty.py
 
 import torch
 import torch.nn as nn
@@ -22,26 +22,11 @@ class Generator(torch.nn.Module):
         # Input_dim = 100
         # Output_dim = C (number of channels)
         self.main_module = nn.Sequential(
-            # Z latent vector 100
-            nn.ConvTranspose2d(in_channels=100, out_channels=1024, kernel_size=4, stride=1, padding=0),
-            nn.BatchNorm2d(num_features=1024),
-            nn.ReLU(True),
-
-            # State (1024x4x4)
-            nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(num_features=512),
-            nn.ReLU(True),
-
-            # State (512x8x8)
-            nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(num_features=256),
-            nn.ReLU(True),
-
-            # State (256x16x16)
-            nn.ConvTranspose2d(in_channels=256, out_channels=channels, kernel_size=4, stride=2, padding=1))
+            nn.Linear(312+100, 2048),
+        )
             # output of main module --> Image (Cx32x32)
 
-        self.output = nn.Tanh()
+        self.output = nn.ReLU()
 
     def forward(self, x):
         x = self.main_module(x)
@@ -55,28 +40,13 @@ class Discriminator(torch.nn.Module):
         # Input_dim = channels (Cx64x64)
         # Output_dim = 1
         self.main_module = nn.Sequential(
-            # Omitting batch normalization in critic because our new penalized training objective (WGAN with gradient penalty) is no longer valid
-            # in this setting, since we penalize the norm of the critic's gradient with respect to each input independently and not the enitre batch.
-            # There is not good & fast implementation of layer normalization --> using per instance normalization nn.InstanceNorm2d()
-            # Image (Cx32x32)
-            nn.Conv2d(in_channels=channels, out_channels=256, kernel_size=4, stride=2, padding=1),
-            nn.InstanceNorm2d(256, affine=True),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            # State (256x16x16)
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1),
-            nn.InstanceNorm2d(512, affine=True),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            # State (512x8x8)
-            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=4, stride=2, padding=1),
-            nn.InstanceNorm2d(1024, affine=True),
-            nn.LeakyReLU(0.2, inplace=True))
+            nn.Linear(2048, 1)
+        )
             # output of main module --> State (1024x4x4)
 
         self.output = nn.Sequential(
-            # The output of D is no longer a probability, we do not apply sigmoid at the output of D.
-            nn.Conv2d(in_channels=1024, out_channels=1, kernel_size=4, stride=1, padding=0))
+            nn.LeakyReLU(0.02)
+        )
 
 
     def forward(self, x):
